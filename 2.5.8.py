e0fbe60f5c8d41d6771ab7ff0f76a6b9f85c9ee9
Evgeniia Mestnikova <33741246+jombaek@users.noreply.github.com>
2023-09-26 08:41:49 +0000

    2.5.8

---
+++ 2.5.8.py
@@ -0,0 +1,10 @@
+n = int(input())
+c = n % 10
+b = n // 10 % 10
+a = n // 100
+print(a, b, c, sep='')
+print(a, c, b, sep='')
+print(b, a, c, sep='')
+print(b, c, a, sep='')
+print(c, a, b, sep='')
+print(c, b, a, sep='')
