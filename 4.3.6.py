0882223ddc5937ddf7a652c0f45a940717b1f267
Evgeniia Mestnikova <33741246+jombaek@users.noreply.github.com>
2023-10-06 03:48:35 +1000

    4.3.6

---
+++ 4.3.6.py
@@ -0,0 +1,17 @@
+a = int(input())
+b = int(input())
+s = input()
+
+if s == '+':
+    print(a + b)
+elif s == '-':
+    print(a - b)
+elif s == '*':
+    print(a * b)
+elif s == '/':
+    if b == 0:
+        print('На ноль делить нельзя!')
+    else:
+        print(a / b)
+else:
+    print('Неверная операция')
