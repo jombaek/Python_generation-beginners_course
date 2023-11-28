30a863d5115764fcde22e3ff5a45066e79454bb2
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-11-28 22:32:25 +0600

    13.2.3

---
+++ 13. Functions/13.2 Functions with parameters/13.2.3.py
@@ -0,0 +1,15 @@
+# объявление функции
+def print_digit_sum(num):
+    total = 0
+
+    while num:
+        total += num % 10
+        num //= 10
+
+    print(total)
+
+# считываем данные
+n = int(input())
+
+# вызываем функцию
+print_digit_sum(n)
