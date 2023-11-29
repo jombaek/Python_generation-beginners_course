6b59524ef5d5e4564a237440761579c9064b7700
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-11-29 21:49:48 +0600

    13.4.5

---
+++ 13. Functions/13.4 Functions with return value. Part 1/13.4.5.py
@@ -0,0 +1,16 @@
+# объявление функции
+def find_all(target, symbol):
+    result = []
+
+    for i in range(len(target)):
+        if target[i] == symbol:
+            result.append(i)
+
+    return result
+
+# считываем данные
+s = input()
+char = input()
+
+# вызываем функцию
+print(find_all(s, char))
