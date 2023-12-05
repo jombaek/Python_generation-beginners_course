2e4404ef65d4bd1b8891afaad93ecfae1947a672
Evgeniia Mestnikova <zhenyamestnik@gmail.com>
2023-12-05 12:39:57 +0600

    13.6.1

---
+++ 13. Functions/13.6 Functions with return value. Part 3/13.6.1.py
@@ -0,0 +1,14 @@
+# объявление функции
+def get_middle_point(x1, y1, x2, y2):
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+
+    return x, y
+
+# считываем данные
+x_1, y_1 = int(input()), int(input())
+x_2, y_2 = int(input()), int(input())
+
+# вызываем функцию
+x, y = get_middle_point(x_1, y_1, x_2, y_2)
+print(x, y)
